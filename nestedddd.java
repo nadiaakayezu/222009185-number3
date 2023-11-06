e49decad5556b7de82a97b6d43173e4e77f142e3
nadiaakayezu <149729827+nadiaakayezu@users.noreply.github.com>
2023-11-06 18:19:45 +0200

    Update nestedddd.java

--- nestedddd.java
+++ nestedddd.java
@@ -1,12 +1,12 @@
-public class nestedddd {
+public class nestedddd {// declare a public class named "nestedddd".
     public static void main(String[] args) {
-        int size = 6;
+        int size = 6;// declare integer variable size and declare it's value which is 6.
 
-        for (int i = 1; i <= size; i++) {
+        for (int i = 1; i <= size; i++) { //Start a loop where "i" ranges from 1 to "size".
             for (int j = 1; j <= size; j++) {
                 System.out.print("$");
             }
-            System.out.println();
+            System.out.println();//move to the bnext line after printing the dolar signs in the inner looop.
         }
     }
 }
