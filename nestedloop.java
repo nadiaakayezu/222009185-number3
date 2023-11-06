6f54a9992d74c30254e7bf8020e2660a2ec25ad9
nadiaakayezu <149729827+nadiaakayezu@users.noreply.github.com>
2023-11-06 18:23:01 +0200

    Update nestedloop.java

--- nestedloop.java
+++ nestedloop.java
@@ -1,10 +1,10 @@
-public class nestedloop {
+public class nestedloop { //This line defines the start of a Java class named "nestedloop."
     public static void main(String[] args) {
-        for(int i=1;i<=10;i++){
+        for(int i=1;i<=10;i++){// If i is equal to 5, the break statement is executed, which immediately exits the loop.
             if(i==5){
                 break;
             }
-            System.out.println(i);
+            System.out.println(i);// the message to be printed or shown in output.
         }
     }
 }
