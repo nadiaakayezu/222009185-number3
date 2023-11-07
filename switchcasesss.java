7c612696bdcc9e807e4f3710f9a1fcb6094967d2
nadiaakayezu <149729827+nadiaakayezu@users.noreply.github.com>
2023-11-07 12:28:54 +0200

    Add files via upload

---
+++ switchcasesss.java
@@ -0,0 +1,44 @@
+public class switchcasesss { // show java experiment name which is "switchcasesss"
+    
+        public static void main(String[] args) {
+            int dayNumber = 3; // this line declare integer to be used in this java experiment called daynumber 
+            switch (dayNumber) {// starting that days of week from monday up to sunday 
+                case 1:
+                    System.out.println("It's Monday");
+                    break;
+                case 2:
+                    System.out.println("It's Tuesday");
+                    break;
+                case 3:
+                    System.out.println("It's Wednesday");
+                    break;
+                case 4:
+                    System.out.println("It's Thursday");
+                    break;
+                case 5:
+                    System.out.println("It's Friday");
+                    break;
+                case 6:
+                    System.out.println("It's Saturday");
+                    break;
+                case 7:
+                    System.out.println("It's Sunday");
+                    break;
+                default:
+                    System.out.println("Invalid day number"); // the message to be sisplayed to output shown here 
+                    break;
+            }
+        }
+    }
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
