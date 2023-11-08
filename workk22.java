2b0e4ec125b10b42972fed1209b7d752c43a8d8b
nadiaakayezu <149729827+nadiaakayezu@users.noreply.github.com>
2023-11-08 22:13:12 +0200

    Update workk22.java

--- workk22.java
+++ workk22.java
@@ -1,42 +1,44 @@
-class  eating {
-    void  table1(){
-        System.out.println(" mango");
-        System.out.println(" apple");
-        System.out.println(" donats");
+ class eating { // declare 
+    void table1(){
+        System.out.println("mango");
+        System.out.println("orange");
+        System.out.println("apple");
         
     }
 }
 class pays extends  eating{
 public void  table2(){
-    System.out.println(" banana");
-    System.out.println(" rice");
-    System.out.println(" omlet");
+    System.out.println("rice");
+    System.out.println("beans");
+    System.out.println("potatoes");
 }
 }
-class Land extends pays{
+class  feed extends pays{
     void  table3(){
-        System.out.println(" soyasos");
-        System.out.println(" tanaty");
-        System.out.println(" sanajuice");
+        System.out.println("water");
+        System.out.println("beer");
+        System.out.println("wine");
     }
 }
-interface landa{
-    void   eating();
+interface  feeds{
+    void  table4();
 }
-class t extends eating implements landa{
+class F extends  feed implements feeds{
    public void  table4(){
-    System.out.println(" banana");
-    System.out.println(" juice");
-    System.out.println(" passion");
+    System.out.println("suchi");
+    System.out.println("bagga");
+    System.out.println("bread");
 }
 }
-public class  workk22 {
+public class class2 {
     public static void main(String[] args) {
-        t   table= new t();
-         table. table1();
-         table. eating();
-         table. eating();
-         table. table4();
+        F  eat= new F();
+         eat.table1();
+         eat.table2();
+         eat.table3();
+         eat.table4();
 
     }
 }
+         
+ 
